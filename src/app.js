3b9b5f3bc14c9c1937f76451311cc8f4876ab1b4
senecaSparsh <88911370+senecaSparsh@users.noreply.github.com>
2023-09-17 17:40:55 -0400

    Added route directory to put all of our server's routes into seperate file and folder to avoid having src/app.js get too big

--- src/app.js
+++ src/app.js
@@ -31,20 +31,7 @@ app.use(compression());
 
 // Define a simple health check route. If the server is running
 // we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
-
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // TODO: change this to use your GitHub username!
-    githubUrl: 'https://github.com/senecaSparsh/fragmants.git',
-    version,
-  });
-});
+app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
